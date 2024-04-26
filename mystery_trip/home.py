7ab9a8eca2769204641eb855417239a0051b10ac
Andrea Waltlova <andywaltlova@gmail.com>
2024-04-26 15:16:35 +0200

    Fix images and counter
    Signed-off-by: Andrea Waltlova <andywaltlova@gmail.com>

--- mystery_trip/home.py
+++ mystery_trip/home.py
@@ -1,7 +1,6 @@
 from flask import (
     Blueprint, render_template
 )
-from werkzeug.exceptions import abort
 
 from datetime import datetime
 
@@ -9,20 +8,17 @@ from datetime import datetime
 bp = Blueprint('home', __name__)
 
 
+
 @bp.route("/")
 def index():
     from . import db
     now = datetime.now()
     target_date = datetime(2024, 5, 22)
+    final_message = db.get_db().execute("SELECT message_text FROM messages WHERE id = 10").fetchone()['message_text']
     if now < target_date:
-        return render_template(
-            'index.html',
-            target_date=target_date,
-            final_message="The mystery trip is still a mystery!"
-        )
-    final_message = db.get_db().execute("SELECT message_text FROM messages WHERE id = 10").fetchone()
+        final_message = "The flag is one refresh away!"
     return render_template(
         'index.html',
         target_date=target_date,
-        final_message=final_message['message_text']
+        final_message=final_message
     )

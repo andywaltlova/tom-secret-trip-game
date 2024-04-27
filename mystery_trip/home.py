b7275b032e851fa527eef46cb95449f10ba9e25c
Andrea Waltlova <andywaltlova@gmail.com>
2024-04-27 13:43:58 +0200

    fix
    Signed-off-by: Andrea Waltlova <andywaltlova@gmail.com>

--- mystery_trip/home.py
+++ mystery_trip/home.py
@@ -14,9 +14,11 @@ def index():
     from . import db
     now = datetime.now()
     target_date = datetime(2024, 5, 22)
-    final_message = db.get_db().execute("SELECT message_text FROM messages WHERE id = 10").fetchone()['message_text']
-    if now < target_date:
+    if now > target_date:
+        final_message = db.get_db().execute("SELECT message_text FROM messages WHERE id = 10").fetchone()['message_text']
+    else:
         final_message = "The flag is one refresh away!"
+
     return render_template(
         'index.html',
         target_date=target_date,
